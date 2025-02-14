7b28c07a8dbf63db5a4b4c30b1eccfe303c44755
Jose Cabrera <josemcabrerat@gmail.com>
2025-02-14 06:38:08 -0700

    I added the POST request within the authAPI.tsx file. Also, I verified that the token exists. I'm still working on returning the JWT token.

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -8,11 +8,11 @@ const login = async (userInfo: UserLogin) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(userInfo),
-  });
+  }); //Within this login function I can make a POST request.
 
   if (!response.ok) {
     throw new Error('Login failed');
-  }
+  }//Here we handle the error if the responde is unsuccessful.
 
   return await response.json();
 }//const login
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,6 +5,22 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    res.sendStatus(401);
+    return;
+  }///This if statement checks if the token is null and if it is, it sends a 401 status code.
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
+    if (err) {
+      res.sendStatus(403);
+      return;
+    }
+    req.user = user as JwtPayload;
+    next();
+  });
 };
